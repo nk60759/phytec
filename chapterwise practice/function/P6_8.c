1acd846fff3077761b0da23e3377775e0db91c98
nk60759 <126573413+nk60759@users.noreply.github.com>
2023-03-21 11:50:08 +0530

    Add files via upload

---
+++ chapterwise practice/function/P6_8.c
@@ -0,0 +1,17 @@
+/*8 Program to find the sum of two numbers*/
+#include<stdio.h>
+int sum(int x,int y)	
+{
+	int s;
+	s=x+y;	
+	return s;
+}
+int main(void)
+{
+	int a,b,s;
+	printf("Enter values for a and b : ");
+	scanf("%d %d",&a,&b);
+	s=sum(a,b);	
+	printf("Sum of %d and %d is %d\n",a,b,s);
+	return 0;
+}
---
+++ chapterwise practice/operators/P4_6.c
@@ -0,0 +1,11 @@
+/*6 Program to print the larger of two numbers using conditional operator */
+#include<stdio.h>
+int main(void)
+{
+	int a,b,max;
+	printf("Enter values for a and b :");	 
+	scanf("%d%d",&a,&b);
+	max = a>b ? a : b;		
+	printf("Larger of %d and %d is %d\n",a,b,max);
+	return 0;
+}
