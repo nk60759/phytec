47c8d9b1abe844341af40ab5d8ef94b77f574306
nk60759 <126573413+nk60759@users.noreply.github.com>
2023-03-21 11:31:13 +0530

    Add files via upload

---
+++ c programming/day8/labq7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void display();
+int main()
+{
+    display();
+    display();
+    return 0;
+
+}
+void display()
+{
+    static int x;
+    x+=10;
+    printf("*\nx=%d",x);
+    
+}
